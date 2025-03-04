b0056481a16fb559695c777b391837452450d8fe
nik <nkvkyk@gmail.com>
2025-03-04 10:54:16 -0600

    playing with array

--- chapter_2/listing_2_11.cpp
+++ chapter_2/listing_2_11.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main() {
   unsigned long maximum = 0;
@@ -7,5 +9,16 @@ int main() {
     if(value > maximum)
       maximum = value;
   }
-  printf("The maximum value is %lu.", maximum);
+  printf("The maximum value is %lu.\n", maximum);
+  short array[] = { 104, 105, 32, 98, 105, 108, 108, 0 };
+  size_t n_elements = sizeof(array) / sizeof(short);
+  for(size_t i = 0; i < n_elements; i++) {
+    cout << array[i] << " ";
+    if (i == (n_elements -1)) cout << endl;
+  }
+  cout << "size of aray in bytes: " << sizeof(array) << endl;
+  cout << sizeof(short) << endl;
+  cout << "number of elements: " << n_elements << endl;
+
+  return 0;
 }
